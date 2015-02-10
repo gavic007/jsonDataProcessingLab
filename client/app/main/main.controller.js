e8abad5ca5dd3222d7b35e1568779c42263b0744
copic006 <copic006@morris.umn.edu>
2015-02-10 13:50:00 -0600

    changed main.controller in terms of student rather than thing.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,26 +2,28 @@
 
 angular.module('jsonDataProcessingLabApp')
   .controller('MainCtrl', function ($scope, $http, socket) {
-    $scope.awesomeThings = [];
+    $scope.students = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
+    $http.get('/api/student').success(function(students) {
+      $scope.students = students;
+      socket.syncUpdates('student', $scope.students);
     });
+    //
+    //$scope.addThing = function() {
+    //  if($scope.newThing === '') {
+    //    return;
+    //  }
+    //  $http.post('/api/things', { name: $scope.newThing });
+    //  $scope.newThing = '';
+    //};
+    //
+    //$scope.deleteThing = function(thing) {
+    //  $http.delete('/api/things/' + thing._id);
+    //};
+    //
+    //$scope.$on('$destroy', function () {
+    //  socket.unsyncUpdates('thing');
+    //});
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
 
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
-
-    $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
-    });
   });
