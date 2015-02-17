e3742a1e4e83681a668101c007643e6f00283604
tylerlemke <lemke164@umn.edu>
2015-02-17 13:19:11 -0600

    tried adding credit totals to students

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -9,6 +9,9 @@ angular.module('jsonDataProcessingLabApp')
     $http.get('/api/student').success(function(students) {
       $scope.students = students;
       socket.syncUpdates('student', $scope.students);
+      for(var i=0; i<$scope.students.length; i++){
+          $scope.students[i].push({totalCredits: $scope.addCredits($scope.students[i])});
+      }
     });
 
 
